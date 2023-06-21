9cdb7d416cd6e4547ed7a751c6261b06f05bebd8
fengkx <liangkx8237@gmail.com>
2023-06-21 22:49:14 +0800

    init

---
+++ src/commands/hello.ts
@@ -0,0 +1,28 @@
+import {Command, Flags} from '@oclif/core'
+
+export default class Hello extends Command {
+  static description = 'describe the command here'
+
+  static examples = [
+    '<%= config.bin %> <%= command.id %>',
+  ]
+
+  static flags = {
+    // flag with a value (-n, --name=VALUE)
+    name: Flags.string({char: 'n', description: 'name to print'}),
+    // flag with no value (-f, --force)
+    force: Flags.boolean({char: 'f'}),
+  }
+
+  static args = [{name: 'file'}]
+
+  public async run(): Promise<void> {
+    const {args, flags} = await this.parse(Hello)
+
+    const name = flags.name ?? 'world'
+    this.log(`hello ${name} from /Users/fengkx/me/code/beancount-smart-query/src/commands/hello.ts`)
+    if (args.file && flags.force) {
+      this.log(`you input --force and --file: ${args.file}`)
+    }
+  }
+}
